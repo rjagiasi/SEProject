0949d3d3b6242da09a5fd19d57926bf9406e9233
Rohan <rohan@localhost.localdomain>
2017-04-04 21:03:00 +0530

    Arpita date part added

--- html/test_page.php
+++ html/test_page.php
@@ -1,1 +1,15 @@
 //Arpita
+<link rel="stylesheet" type="text/css" href="../css/jquery.countdown.css"> 
+<script type="text/javascript" src="../js/jquery.plugin.min.js"></script> 
+<script type="text/javascript" src="../js/jquery.countdown.min.js"></script>
+<div id="timer" style="height:50px;"></div>
+<script type="text/javascript">
+	var d1 = new Date ();
+	var d2 = new Date ( d1 );
+	d2.setHours ( d1.getHours() + 1 );
+	$('#timer').countdown({
+		until: d2, 
+		onExpiry : function () {
+			//what to do when timer goes off
+		}}); 
+</script>
